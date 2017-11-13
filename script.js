f32d869ef40e629ef350d52b7946cd39114b8155
Kyle Treman <jkt290@nyu.edu>
2017-11-13 11:37:37 -0500

    events & adding via scripts

---
+++ script.js
@@ -0,0 +1,6 @@
+// document.querySelector('.fun').innerHTML = "<img src='https://i.pinimg.com/736x/38/f6/60/38f660229b293712a183a561cf0bd6c1--unicorn-club-the-unicorn.jpg' alt='unicorn'>"
+
+document.querySelector('p').addEventListener('click', function(){
+  document.querySelector('.fun').innerHTML = "<img src='https://ibhuluimcom-a.akamaihd.net/ib.huluim.com/show/22881?region=US&size=952x536' alt='Rick&Morty'>"
+
+});
